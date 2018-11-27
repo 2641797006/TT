6dcb41f594e0b046558fb81569ca1c3e53cb96a8
PinkHat <2641797006@qq.com>
2018-11-27 15:46:38 +0800

    -

--- tt.c
+++ tt.c
@@ -1,10 +1,39 @@
 #include <stdio.h>
+//#include <C:\Users\26417\Desktop\stack.h>
+#include <C:\Users\26417\Desktop\SqStack\pSqStackX.h>
 int main(){
-	float pi=3.1415926, r, s, v;
-	printf("请输入球的半径: ");
-	scanf("%f", &r);
-	s=4*pi*r*r;
-	v=4.0/3*pi*r*r*r;
-	printf("球的表面积: %f\n", s);
-	printf("球的体积: %f\n", v);
+InitStack();
+	int a1, a2;
+	float f1, f2;
+	char c1, c2;
+
+	a1=666;
+	a2=0;
+
+	printf("a2=%d\n", a2);
+	push(a1);
+PrintStack();
+	pop(&a2);
+	printf("a2=%d\n", a2);
+
+	f1=3.14159;
+	f2=0;
+PrintStack();
+	printf("f2=%f\n", f2);
+	push(f1);
+PrintStack();
+	pop(&f2);
+PrintStack();
+	printf("f2=%f\n", f2);
+
+	c1='A';
+	c2='0';
+
+	printf("c2=%c\n", c2);
+	push(c1);
+PrintStack();
+	pop(&c2);
+PrintStack();
+	printf("c2=%c\n", c2);
+
 }

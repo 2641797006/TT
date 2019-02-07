a7e5b6275929c3b92c9595dde3e28480f32e2970
cpplay <2641797006@qq.com>
2019-02-07 16:08:38 +0800

    -

---
+++ calculator_lex/token.h
@@ -0,0 +1,25 @@
+#ifndef _TOKEN_H_
+#define _TOKEN_H_
+
+typedef enum{
+	BAD_TOKEN,
+	NUMBER_TOKEN,
+	ADD_OPERATOR_TOKEN,
+	SUB_OPERATOR_TOKEN,
+	MUL_OPERATOR_TOKEN,
+	DIV_OPERATOR_TOKEN,
+	END_OF_LINE_TOKEN
+} TokenKind;
+
+#define MAX_TOKEN_SIZE (100)
+
+typedef struct{
+	TokenKind	kind;
+	double		value;
+	char		str[MAX_TOKEN_SIZE];
+} Token;
+
+void set_line(char *line);
+void get_token(Token *token);
+
+#endif

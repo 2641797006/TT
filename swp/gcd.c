2728788fd85c66f98efe8b4037201ade67480517
cpplay <2641797006@qq.com>
2019-04-08 09:29:29 +0800

    gcd

--- swp/gcd.c
+++ swp/gcd.c
@@ -4,3 +4,13 @@ long long gcd( long long a, long long b )
 	return b ? gcd(b, a%b) : a;
 }
 
+#include <stdio.h>
+
+int
+main()
+{
+	long long a, b;
+	scanf("%lld%lld", &a, &b);
+	printf("gcd(%lld,%lld)=%lld\n", a, b, gcd(a,b));
+	return 24;
+}

9727e121b99047ff979bf6e9c5695e0a5386febe
cpplay <2641797006@qq.com>
2019-04-11 22:20:21 +0800

    -

--- cpp/hello.cpp
+++ cpp/hello.cpp
@@ -1,16 +1,10 @@
 #include <iostream>
 using namespace std;
 
-auto f(void())->void;
-
 int
 main()
 {
-	f( []()->void { cout<<"hello world"<<endl; } );
+	[] () -> void { cout<<"hello world"<<endl; } ();
 	return 24-'k';
 }
 
-auto f(void f())->void
-{
-	f();
-}

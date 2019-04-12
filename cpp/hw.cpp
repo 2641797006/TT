741004f9ebf3f4ff8464bb02922a877cad6c10c0
cpplay <2641797006@qq.com>
2019-04-12 12:26:36 +0800

    -

--- cpp/hw.cpp
+++ cpp/hw.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
-using namespace std;
 
 class main{ public: main(); };
 class _{ public: _(){ main main; } }_;
 auto main()->int{ return 24-'k'; }
+
+using namespace std;
 
 main::main()
 {

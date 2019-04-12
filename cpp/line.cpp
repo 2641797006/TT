18bae1c62c17a1342718d1e8540406259dbc8c84
cpplay <2641797006@qq.com>
2019-04-12 13:40:44 +0800

    -

---
+++ cpp/line.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+
+class main{ public: main(); };
+class _{ ::main main; }_;
+auto main()->int{ return 24-'k'; }
+
+using namespace std;
+
+class Point{
+  public:
+	double x, y;
+};
+
+main::main()
+{
+	Point A = { 0, 70};
+	Point B = { 70, 0};
+	double x = B.x - A.x;
+	double y = B.y - A.y;
+	double k = y/x;
+	double b = A.y - k * A.x;
+
+	cout<<"斜率k="<<k<<endl;
+	cout<<"直线方程: "<<k<<" * X - Y = "<<-b<<endl;
+}
+
